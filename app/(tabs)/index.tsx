8c3cd860b859982b939f566c1cd25b6fe8cf8473
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-12 19:21:13 +0200

    install and use apollo-client and graphql with a test query

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,57 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { FlatList, Platform, StyleSheet, Text, View } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const QUERY = gql`
+  query TestQuery {
+    allCities {
+      id
+      name
+    }
+  }
+`;
+
+type QueryType = {
+  allCities: [
+    {
+      id: number;
+      name: string;
+    },
+  ];
+};
 
 export default function HomeScreen() {
+  const { data, error } = useQuery<QueryType>(QUERY);
+
+  if (error) {
+    console.log(error.name, error.message);
+    return null;
+  }
+
+  if (data) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList
+          data={data.allCities}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{`${item.name}`}</Text>
+            </View>
+          )}
+          keyExtractor={(city: any) => `${city.id}`}
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -24,8 +68,9 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          Edit{' '}
+          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{' '}
+          to see changes. Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
@@ -43,7 +88,11 @@ export default function HomeScreen() {
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
+            <Link.MenuAction
+              title="Action"
+              icon="cube"
+              onPress={() => alert('Action pressed')}
+            />
             <Link.MenuAction
               title="Share"
               icon="square.and.arrow.up"
@@ -68,8 +117,11 @@ export default function HomeScreen() {
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">
+            npm run reset-project
+          </ThemedText>{' '}
+          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{' '}
+          directory. This will move the current{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import Constants from 'expo-constants';
+
+export function getGraphqlUrl(port = 4000) {
+  const debuggerHost =
+    Constants.expoConfig?.hostUri ||
+    Constants.manifest2?.extra?.expoGo?.debuggerHost;
+  const host = debuggerHost?.split(':')[0];
+  return `http://${host}:${port}/graphql`;
+}
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: getGraphqlUrl() }),
+  cache: new InMemoryCache(),
+});
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,12 +33,17 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: 'modal', title: 'Modal' }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
