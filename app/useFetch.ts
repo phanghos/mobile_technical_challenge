99466dea88910ec366386b2161b036512307851c
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-13 10:21:41 +0200

    create wrapper over useQuery calls to decouple graphql from components

---
+++ app/useFetch.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/client/react';
+import { DocumentNode } from 'graphql';
+
+type UseFetchReturn<T> = {
+  data: T | undefined;
+  loading: boolean;
+  error: Error | undefined;
+};
+
+export const useFetch = <T>(query: DocumentNode): UseFetchReturn<T> => {
+  const { data, loading, error } = useQuery<T>(query);
+
+  return {
+    data: data ?? undefined,
+    loading,
+    error: error ?? undefined,
+  };
+};
