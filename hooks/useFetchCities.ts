9a2adf0ec5d615e34a9319eff85be20a1c176f25
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-13 13:04:52 +0200

    use language and currency helpers when fetching cities in useFetchCities

--- hooks/useFetchCities.ts
+++ hooks/useFetchCities.ts
@@ -1,5 +1,21 @@
 import type { AllCities } from '@/app/core/entities/AllCities';
+import { City } from '@/app/core/entities/City';
+import { CurrencyUtils } from '@/app/core/utils/currency';
+import { LanguageUtils } from '@/app/core/utils/language';
 import { GET_CITIES } from '@/app/graphql/queries';
-import { useFetch } from './useFetch';
+import { useFetch, UseFetchReturn } from './useFetch';
 
-export const useFetchCities = () => useFetch<AllCities>(GET_CITIES);
+export const useFetchCities = (): UseFetchReturn<City[]> => {
+  const result = useFetch<AllCities>(GET_CITIES);
+
+  return {
+    ...result,
+    data: result.data?.allCities.map(it => {
+      return {
+        ...it,
+        language: LanguageUtils.getLanguage(it.language),
+        currency: CurrencyUtils.getCurrency(it.currency),
+      };
+    }),
+  };
+};
