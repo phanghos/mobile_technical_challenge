68fddd56790e899d8f43588949ed3b1db0ee4984
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-14 12:46:17 +0200

    add places utils to easily get either a map or array of places for a given city

---
+++ app/core/utils/places.ts
@@ -0,0 +1,30 @@
+import { Place, PlaceInfo } from '../entities/Place';
+import { PlacesMap } from '../entities/PlacesMap';
+import { PlaceType } from '../entities/PlaceType';
+
+const getPlacesForCityByType = (
+  cityKey: string,
+  places: Place[],
+): PlacesMap => {
+  return places.reduce((acc, cur) => {
+    if (cur.key !== cityKey) {
+      return acc;
+    }
+
+    return {
+      ...acc,
+      [cur.place.type]: [...(acc[cur.place.type] || []), cur.place],
+    };
+  }, {} as PlacesMap);
+};
+
+const getAllPlaces = (placesMap: PlacesMap): PlaceInfo[] =>
+  Object.keys(placesMap).reduce(
+    (acc, cur) => [...acc, ...placesMap[cur as PlaceType]],
+    [] as PlaceInfo[],
+  );
+
+export const PlacesUtils = {
+  getPlacesForCityByType,
+  getAllPlaces,
+};
