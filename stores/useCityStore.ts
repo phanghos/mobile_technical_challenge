ace64025ee6b0a3d46684f2169156dfadb244b5b
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-14 19:18:33 +0200

    add persistence to city store by leveraging zustans persist middleware

--- stores/useCityStore.ts
+++ stores/useCityStore.ts
@@ -1,12 +1,22 @@
 import type { City } from '@/app/core/entities/City';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type CityStoreState = {
   cities: City[];
 };
 
-export const useCityStore = create<CityStoreState>(() => ({
-  cities: [],
-}));
+export const useCityStore = create<CityStoreState>()(
+  persist(
+    () => ({
+      cities: [] as City[],
+    }),
+    {
+      name: 'city-store',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 
 export type CityStore = typeof useCityStore;
