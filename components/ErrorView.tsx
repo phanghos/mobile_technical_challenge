293d41f67510e23cd6937ce1003712e6b51c863c
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-14 14:08:35 +0200

    add ErrorView component to handle error states

---
+++ components/ErrorView.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { EmptyList } from './EmptyList';
+
+type ErrorViewProps = {
+  ctaText?: string;
+  onPress?: () => void;
+} & React.ComponentProps<typeof EmptyList>;
+
+export const ErrorView = ({
+  title,
+  description,
+  ctaText,
+  onPress,
+}: ErrorViewProps) => {
+  const shouldRenderButton = Boolean(ctaText && onPress);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+      {shouldRenderButton && (
+        <Button mode="outlined" onPress={onPress} style={{ marginTop: 16 }}>
+          {ctaText}
+        </Button>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 4,
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: 300,
+    textAlign: 'center',
+  },
+});
